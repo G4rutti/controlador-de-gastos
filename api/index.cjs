fa085ee0819aa4397a3bd51737160d97a74d2229
G4rutti <davigarutti5@gmail.com>
2023-02-21 15:58:10 -0300

    agora vai?

--- api/index.cjs
+++ api/index.cjs
@@ -24,18 +24,15 @@ app.use(express.static('public'));
 
 // Rota principal
 app.get("/", async (req, res) =>{
-    mongoose.connect(`mongodb+srv://davigarutti5:BFiP1LTgvWe7GQL1@cluster0.3gunjkh.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true })
     res.send("hello world")
 });
 
 app.get("/ler", async (req, res) => {
-    mongoose.connect(`mongodb+srv://davigarutti5:BFiP1LTgvWe7GQL1@cluster0.3gunjkh.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true })
     const cursor = await Financas.find({})
     return res.send(cursor)
 });
 
 app.post("/criar", async(req, res) => {
-    mongoose.connect(`mongodb+srv://davigarutti5:BFiP1LTgvWe7GQL1@cluster0.3gunjkh.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true })
     const { id, tipo, descricao, valor } = req.body;
     const gasto = new Financas({ id: id, tipo: tipo, descricao: descricao, valor: valor });
     gasto.save(function (err) {
@@ -46,7 +43,6 @@ app.post("/criar", async(req, res) => {
 })
 
 app.delete("/deletar/:id", async(req, res) =>{
-    mongoose.connect(`mongodb+srv://davigarutti5:BFiP1LTgvWe7GQL1@cluster0.3gunjkh.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true })
     try {
         const id = req.params.id;
         await Financas.deleteOne({id:id});
@@ -59,16 +55,16 @@ app.delete("/deletar/:id", async(req, res) =>{
 })
 
 
-app.listen(PORT)
-console.log('Conectado')
+// app.listen(PORT)
+// console.log('Conectado')
 // Entregar uma porta
-// const DB_USER = process.env.DB_USER
-// const DB_PASSWORD = process.env.DB_PASSWORD
-// mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.3gunjkh.mongodb.net/?retryWrites=true&w=majority`)
-// .then(() => {
-//     app.listen(PORT)
-//     console.log('Conectado')
-// })
-// .catch((error) => {
-//     console.log(error)
-// })
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.3gunjkh.mongodb.net/?retryWrites=true&w=majority`,{ useNewUrlParser: true })
+.then(() => {
+    app.listen(PORT)
+    console.log('Conectado')
+})
+.catch((error) => {
+    console.log(error)
+})
